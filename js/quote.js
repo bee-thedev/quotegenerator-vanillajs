11a64e1af356ce871594db7b6384c0f733df66b6
Beenysh Sara <beenyshsaeed@gmail.com>
2022-02-01 23:19:42 +0500

    Api URL changed

--- js/quote.js
+++ js/quote.js
@@ -5,19 +5,29 @@ authorName = document.querySelector(".quoteAuthor");
 speechButton = document.querySelector(".quoteSpeech");
 copyButton = document.querySelector(".quoteCopy");
 twitterButton = document.querySelector(".twitter");
+const apiurl = "https://goquotes-api.herokuapp.com/api/v1/random?count=300";
 
+console.log(apiurl)
 synth = speechSynthesis;
 
 function fetchQuote(){
     quoteButton.classList.add("loading");
     quoteButton.innerText = "Loading Quote ....";
-    fetch("http://api.quotable.io/random").then((response=>response.json())).then(result=>{
-        quoteText.innerText = result.content;
-        authorName.innerText = result.author;
-        quoteButton.classList.remove("loading");
-        quoteButton.innerText = "New Quote";
-        }
-    )
+    // fetch("https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=10", {
+    //     "method": "GET",
+    //     "headers": {
+    //         "x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
+    //         "x-rapidapi-key": "657a1e36c2mshfb425b63c1e6113p151077jsn2285ffdb2957"
+    //     }
+    // })
+    fetch(apiurl)
+    .then((response=>response.json())).then(result=>
+        result.quotes.forEach((data)=> {
+            quoteText.innerText = data.text;
+            authorName.innerText = data.author;
+            quoteButton.classList.remove("loading");
+            quoteButton.innerText = "New Quote";
+        }))
 }
     
 // ----Text to Speech(synthesis) Function 
