fb8a02a1bdef8110154fb3d30e6b53575bc137cf
Beenysh Sara <beenyshsaeed@gmail.com>
2022-02-02 11:55:17 +0500

    Code cleaned

--- js/quote.js
+++ js/quote.js
@@ -5,7 +5,9 @@ authorName = document.querySelector(".quoteAuthor");
 speechButton = document.querySelector(".quoteSpeech");
 copyButton = document.querySelector(".quoteCopy");
 twitterButton = document.querySelector(".twitter");
-const apiurl = "https://goquotes-api.herokuapp.com/api/v1/random?count=300";
+const apiurl = "https://goquotes-api.herokuapp.com/api/v1/random?count=200";
+
+// const apiurl ="http://api.quotable.io/random";
 
 console.log(apiurl)
 synth = speechSynthesis;
@@ -13,13 +15,6 @@ synth = speechSynthesis;
 function fetchQuote(){
     quoteButton.classList.add("loading");
     quoteButton.innerText = "Loading Quote ....";
-    // fetch("https://andruxnet-random-famous-quotes.p.rapidapi.com/?cat=famous&count=10", {
-    //     "method": "GET",
-    //     "headers": {
-    //         "x-rapidapi-host": "andruxnet-random-famous-quotes.p.rapidapi.com",
-    //         "x-rapidapi-key": "657a1e36c2mshfb425b63c1e6113p151077jsn2285ffdb2957"
-    //     }
-    // })
     fetch(apiurl)
     .then((response=>response.json())).then(result=>
         result.quotes.forEach((data)=> {
